f767082bd26c3d497cfca3ed06ecab3dc29721ab
Md Nayem Khan <samratk0145@gmail.com>
2026-02-17 13:51:02 +0600

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: process.env.VITE_BASE_PATH || "/papeleducare",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
